aba1b03aad05dfed5bb73f3f02ba9a629515479b
bishal <bishalbhaandari62@gmail.com>
2024-11-05 09:17:25 +0100

    -Visualization added for cluster -More cluster added

--- cluster_busstop.py
+++ cluster_busstop.py
@@ -1,34 +1,48 @@
 import pandas as pd
 import plotly.express as px
 
-# Load your CSV file (update the path if needed)
-df = pd.read_excel('/path/to/your_file.ods', engine='odf')
+# File upload
+df = pd.read_excel('refineData/BusLine_And_Stops.ods', engine='odf')
 
 # Group by 'Bus_Stop' and count the unique bus lines for each stop
 bus_stop_groups_detailed = df.groupby('Bus_Stop').agg({
     'Bus_Line': lambda x: list(x.unique())  # List of unique bus lines for each stop
 }).reset_index()
 
-# Count the number of lines each bus stop serves
+# Counting the bus line based on stop
 bus_stop_groups_detailed['Line_Count'] = bus_stop_groups_detailed['Bus_Line'].apply(len)
 
 
-# Define clusters based on the range of bus lines served
+# For cluster based on the bus line
 def assign_cluster(line_count):
     if line_count == 1:
         return '1 Line'
     elif line_count == 2:
         return '2 Lines'
+    elif line_count == 3:
+        return '3 Lines'
+    elif line_count == 4:
+        return '4 Lines'
+    elif line_count == 5:
+        return '5 Lines'
+    elif line_count == 6:
+        return '6 Lines'
+    elif line_count == 7:
+        return '7 Lines'
+    elif line_count == 8:
+        return '8 Lines'
+    elif line_count == 9:
+        return '9 Lines'
     else:
-        return '3+ Lines'
+        return '10+ Lines'
 
 
 bus_stop_groups_detailed['Cluster'] = bus_stop_groups_detailed['Line_Count'].apply(assign_cluster)
 
-# Prepare data for plotting with clusters
+# Plotting for cluster
 bus_stop_groups_detailed['Bus_Lines'] = bus_stop_groups_detailed['Bus_Line'].apply(lambda x: ', '.join(map(str, x)))
 
-# Create an interactive scatter plot
+# For scatter plot
 fig = px.scatter(
     bus_stop_groups_detailed,
     x='Line_Count',
@@ -38,13 +52,13 @@ fig = px.scatter(
     title='Interactive Clustering of Bus Stops by Number of Bus Lines'
 )
 
-# Customize plot appearance
+# Plot label
 fig.update_layout(
-    xaxis_title='Number of Bus Lines Served',
+    xaxis_title='Number of Bus Lines',
     yaxis_title='Bus Stop',
     yaxis={'categoryorder': 'total ascending'},  # Arrange bus stops based on clustering
     title_x=0.5
 )
 
-# Show the interactive plot
+# Show plot
 fig.show()

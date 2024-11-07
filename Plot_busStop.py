6aae6e616edbdeb8452853c684cbf2d005c56c8a
bishal <bishalbhaandari62@gmail.com>
2024-11-07 10:46:27 +0100

    -Visualization with basemap

--- Plot_busStop.py
+++ Plot_busStop.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
@@ -11,3 +12,14 @@ m = Basemap(projection='merc',
             llcrnrlat=df['latitude'].min() - 1, urcrnrlat=df['latitude'].max() + 1,
             llcrnrlon=df['longitude'].min() - 1, urcrnrlon=df['longitude'].max() + 1,
             resolution='i')
+
+m.drawcoastlines()
+m.drawcountries()
+
+# Scatter plot for latitude and longitude with different colors
+colors = plt.cm.rainbow(np.linspace(0, 1, len(df)))
+for i, (lat, lon) in enumerate(zip(df['latitude'], df['longitude'])):
+    x, y = m(lon, lat)
+    m.plot(x, y, 'o', markersize=5, color=colors[i])
+
+plt.show()

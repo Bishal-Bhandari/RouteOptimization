c21525680ed222ae222ac6eda238cfff7e5ae9b7
bishal <bishalbhaandari62@gmail.com>
2024-11-09 15:38:42 +0100

    -Node conversion

--- route_finding.py
+++ route_finding.py
@@ -12,3 +12,12 @@ G = ox.graph_from_point(start_point, dist=5000, network_type='drive')
 # Find the nearest nodes in the graph to the start and end points
 orig_node = ox.distance.nearest_nodes(G, start_point[1], start_point[0])
 dest_node = ox.distance.nearest_nodes(G, end_point[1], end_point[0])
+# Find the shortest path between the nodes
+route = nx.shortest_path(G, orig_node, dest_node, weight='length')
+
+# Convert node coordinates in the route to a list of lat-lng pairs
+route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
+
+# Create a Folium map centered at the midpoint of the route
+midpoint = ((start_point[0] + end_point[0]) / 2, (start_point[1] + end_point[1]) / 2)
+m = folium.Map(location=midpoint, zoom_start=14)

aaaf8949f26835b14baa4b60e1d4d0b19a9d5f3d
bishal <bishalbhaandari62@gmail.com>
2024-11-14 13:41:50 +0100

    -Range for bus stop

--- Check_stop.py
+++ Check_stop.py
@@ -41,6 +41,17 @@ for entry in json_data:
         tooltip=entry['name']  # Display the name on hover
     ).add_to(m)
 
+for entry in json_data:
+    folium.CircleMarker(
+        location=[entry['latitude'], entry['longitude']],
+        radius=5,
+        color='blue',
+        fill=True,
+        fill_color='blue',
+        fill_opacity=0.6,
+        tooltip=entry['name']  # Display the name on hover
+    ).add_to(m)
+
 # Save the map to an HTML file and display it
 map_file = 'templates/coordinates_map.html'
 m.save(map_file)

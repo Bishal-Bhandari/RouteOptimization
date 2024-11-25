30889ef6ff89be5aa6ab3506e37e3a65379a7e51
bishal <bishalbhaandari62@gmail.com>
2024-11-25 11:14:53 +0100

    -Stop data fro BMT

---
+++ Brussel MT/Gtfs.py
@@ -0,0 +1,21 @@
+import json
+
+import requests
+import geopandas as gpd
+
+# Load the API keys from the JSON file
+with open('../api_keys.json') as json_file:
+    api_keys = json.load(json_file)
+
+# Brussel MT API key
+api_key = api_keys['Brussels']['API_key']
+
+url = "https://api.mobilitytwin.brussels/stib/stops"
+
+data = requests.get(url, headers={
+    'Authorization': f'Bearer {api_key}'
+}).json()
+
+gdf = gpd.GeoDataFrame.from_features(data["features"])
+# Plot the GeoDataFrame
+gdf.plot()

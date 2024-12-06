196710f71b45bcb136ea30490e8d234f6be1710b
bishal <bishalbhaandari62@gmail.com>
2024-12-06 12:29:20 +0100

    -Range checked

--- POI/POI_BusStop_RELATION.py
+++ POI/POI_BusStop_RELATION.py
@@ -7,3 +7,17 @@ bus_stop_file = "final_busStop_density.ods"  # Replace with your bus stop file p
 
 poi_data = pd.read_excel(poi_file, engine='odf')
 bus_stop_data = pd.read_excel(bus_stop_file, engine='odf')
+
+
+# Bus stops within a 500-meter radius of a given POI
+def find_nearby_bus_stops(poi, bus_stops, radius=500):
+    poi_location = (poi['lat'], poi['lon'])
+    nearby_stops = []
+
+    for _, bus_stop in bus_stops.iterrows():
+        bus_stop_location = (bus_stop['Latitude'], bus_stop['Longitude'])
+        distance = geodesic(poi_location, bus_stop_location).meters
+        if distance <= radius:
+            nearby_stops.append(bus_stop['name'])
+
+    return nearby_stops
